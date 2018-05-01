a8e26086e79ff7348fc07767f0bd87dce0f95a04
dpaniagua10 <dpaniagua2017@gmail.com>
2018-05-01 11:59:01 -0600

    two commit

--- gt/edu/base/DataBase.java
+++ gt/edu/base/DataBase.java
@@ -1,22 +1,25 @@
 package gt.edu.base;
 
-import java.io.File;
-import java.io.IOException;
-import java.util.Scanner;
+import java.io.*;
+import java.util.*;
 
 public class DataBase {
 
-	final static String declaracion="C:/Users/R&D/Desktop/java/datos1.txt";
-	final static String datos="C:/Users/R&D/Desktop/java/datos2.txt";
+	final static String declaracion="C:/Users/Rosy Perez/Desktop/java/datos1.txt";
+	final static String datos="C:/Users/Rosy Perez/Desktop/java/datos2.txt";
 	static Scanner captura = new Scanner(System.in);
+	File fconfig = new File(declaracion);
+	List<Valores> lista = new ArrayList<Valores>();
 	
 	public static void main(String[] args) {
 		
-		DataBase.menu();//por ser un metodo estatico
-
+		//DataBase.menu();//por ser un metodo estatico
+		DataBase principal = new DataBase();
+		principal.menu();
+		
 	}
 	
-	public static void menu(){
+	public void menu(){
 		byte op;
 		
 		do {
@@ -28,11 +31,11 @@ public class DataBase {
 				System.out.println("Archivo principal ya creado ");
 				System.out.println(" ");
 			}else {
-				System.out.println("Archivo principal no creado");
-				System.out.println("Seleccione crear principal ");
+				System.out.println("Archivo de configuracion, no creado ");
+				System.out.println("Seleccione 1. crear archivo de configuracion ");
 			}
 			
-			System.out.println("\n1. Crear archivo principal ");
+			System.out.println("\n1. Crear archivo de configuracion ");
 			System.out.println("2. Ingresar datos ");
 			System.out.println("3. Buscar registro ");
 			System.out.println("4. Listar registros ");
@@ -43,10 +46,11 @@ public class DataBase {
 			switch(op) {
 				case 1:
 					System.out.println(".....");
-					if(crearPrincipal()) {
-						registrarCampos();
-					}else {
+					if(validarFile()) {
 						System.out.println("Ocurrio un error");
+					}else {
+						registrarCampos();
+						escribirConfig();
 					}
 					break;
 				case 2:
@@ -66,31 +70,57 @@ public class DataBase {
 
 	}
 	
-	public static boolean validarFile() {
+	public boolean validarFile() {
 		File f = new File(declaracion);
 		return f.exists();
 	}
-
-	public static boolean crearPrincipal() {
-		File f = new File(declaracion);
-		try {
-			f.createNewFile();
-			System.out.println("\tArchivo creado exitosamente \n");
-			return true;
-		} catch (IOException e) {
-			System.err.println(e.getMessage());
-			return false;
-		}
-	}
 	
-	public static void registrarCampos() {
+	public void registrarCampos() {
+		
+		Valores x = new Valores();
 		System.out.println("Ingrese la cantidad de campos ");
 		int n=captura.nextInt();
 		for(int i=0;i<n;i++) {
+			captura.nextLine();
+			System.out.println("Ingrese el nombre del campo: ");
+			x.setNombre(captura.nextLine());
+			System.out.println("Seleccione tipo de dato: ");
+			mostrar();
+			x.setDato(captura.nextShort());
 			
-			System.out.println("Ingrese el nombre del campo ");
-			
-			
+			lista.add(x);
+		}
+		
+	}
+	
+	public void mostrar() {
+		
+		System.out.println("1. Alfanumerico ");
+		System.out.println("2. Entero ");
+		System.out.println("3. Decimal ");
+		System.out.println("4. Verdadero/Falso ");
+		System.out.println("5. Fecha dd/mm/aaaa ");
+		System.out.println("6. Moneda Q ");
+		
+	}
+	
+	public void escribirConfig() {
+		
+		DataOutputStream dout = null;
+		try {
+			dout = new DataOutputStream(new FileOutputStream(declaracion,true));
+			for (Valores x : lista) {
+				dout.writeUTF(x.getNombre());
+				dout.writeShort(x.getDato());
+			}
+		} catch (IOException e) {		
+			e.printStackTrace();
+		}finally {
+			try {
+				dout.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
 		}
 		
 	}
